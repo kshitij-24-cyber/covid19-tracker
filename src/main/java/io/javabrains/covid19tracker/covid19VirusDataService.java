239649d899d94b31acc45cbda2d87d5a63f0e4e3
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-04-19 11:50:30 +0530

    initial commit

--- src/main/java/io/javabrains/covid19tracker/covid19VirusDataService.java
+++ src/main/java/io/javabrains/covid19tracker/covid19VirusDataService.java
@@ -5,6 +5,7 @@ package io.javabrains.covid19tracker;
  * Date: 19-04-2022
  */
 
+import io.javabrains.covid19tracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,6 +18,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service  // this is used to mark the method as a service
@@ -25,6 +28,7 @@ public class covid19VirusDataService {
      private  static  String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     // method to fetch data from the file
 
+    private List<LocationStats> allStats = new ArrayList<>();
 
     @PostConstruct   // This is used to tell the spring that execute this method when the application start
     @Scheduled(cron = "* * * * * * ")
@@ -32,6 +36,7 @@ public class covid19VirusDataService {
     // this '*' -> shows second,minute,hour,day,month,year
 
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
         // making the get method to get the data
         HttpClient client = HttpClient.newHttpClient();
         // making request from the sever

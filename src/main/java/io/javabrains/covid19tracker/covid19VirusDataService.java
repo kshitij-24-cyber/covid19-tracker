acb0c135113da602be06a0174a33c93950a49a68
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-04-19 11:45:57 +0530

    initial commit

--- src/main/java/io/javabrains/covid19tracker/covid19VirusDataService.java
+++ src/main/java/io/javabrains/covid19tracker/covid19VirusDataService.java
@@ -7,6 +7,7 @@ package io.javabrains.covid19tracker;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -21,27 +22,30 @@ import java.net.http.HttpResponse;
 @Service  // this is used to mark the method as a service
 
 public class covid19VirusDataService {
-
+     private  static  String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     // method to fetch data from the file
 
 
     @PostConstruct   // This is used to tell the spring that execute this method when the application start
+    @Scheduled(cron = "* * * * * * ")
+    // this annotation is used when tha application has to be run on daily bases and has to be updated
+    // this '*' -> shows second,minute,hour,day,month,year
 
     public void fetchVirusData() throws IOException, InterruptedException {
-    // making the get method to get the data
+        // making the get method to get the data
         HttpClient client = HttpClient.newHttpClient();
         // making request from the sever
         // covid-19 data CSV file
-        String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         // ofString -> take the body and just return as a string
-       HttpResponse<String> httpResponse  =client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         // this code will  detect the header from the CSV file and remove it
         // StringReader is a instance of reader which parses string
-        StringReader csvBodyReader  = new StringReader(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
             String state = record.get("Province/State");
---
+++ src/main/java/io/javabrains/covid19tracker/models/LocationStats.java
@@ -0,0 +1,8 @@
+package io.javabrains.covid19tracker.models;/*
+ * Author Name:Kshitij sahu
+ * IDE: intellij IDEA Community Edition
+ * Date: 19-04-2022
+ */
+
+public class LocationStats {
+}

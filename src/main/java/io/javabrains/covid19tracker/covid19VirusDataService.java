20cd9c7c4a6b573e5c26e7d68d7598e0462d5a35
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-04-19 11:18:00 +0530

    initial commit

--- src/main/java/io/javabrains/covid19tracker/covid19VirusDataService.java
+++ src/main/java/io/javabrains/covid19tracker/covid19VirusDataService.java
@@ -1,6 +1,6 @@
 package io.javabrains.covid19tracker;
 /*
- * Author Name:Kshitij sahu
+ * Author Name:Kshitij Sahu
  * IDE: intellij IDEA Community Edition
  * Date: 19-04-2022
  */
@@ -10,9 +10,8 @@ import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -22,8 +21,6 @@ import java.net.http.HttpResponse;
 @Service  // this is used to mark the method as a service
 
 public class covid19VirusDataService {
-    // covid-19 data CSV file
-    private  final  String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     // method to fetch data from the file
 
@@ -34,22 +31,22 @@ public class covid19VirusDataService {
     // making the get method to get the data
         HttpClient client = HttpClient.newHttpClient();
         // making request from the sever
+        // covid-19 data CSV file
+        String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         // ofString -> take the body and just return as a string
        HttpResponse<String> httpResponse  =client.send(request, HttpResponse.BodyHandlers.ofString());
-       // printing the data
-        System.out.println(httpResponse.body());
 
-        // this code will will detect the header from the CSV file and remove it
-        Reader in = new FileReader("path/to/file.csv");
-
-        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+        // this code will  detect the header from the CSV file and remove it
+        // StringReader is a instance of reader which parses string
+        StringReader csvBodyReader  = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String id = record.get("ID");
-            String customerNo = record.get("CustomerNo");
-            String name = record.get("Name");
+            String state = record.get("Province/State");
+            System.out.println(state);
+
         }
 
     }
